51c98efb3fd4bc446ec0f931f9d1ab88b6543043
Shreyas Hande <shreyas@everwell.org>
2023-06-16 12:25:32 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <BarChart />
+  </div>
+</template>
+
+<script>
+import BarChart from '@/containers/BarChart.vue';
+
+ export default {
+   name: 'App',
+   components: {
+     BarChart
+   }
+ }
+</script>
+
