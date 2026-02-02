fd23550af4ed0dd73448a9221d0a1558b7a4a2c1
ShreyasVH <shreyas.deve.1@gmail.com>
2026-02-02 17:13:10 +0530

    color for bar chart

--- src/containers/BarChart.vue
+++ src/containers/BarChart.vue
@@ -55,7 +55,7 @@ export default {
     return {
       chartData: {
         labels: [ 'January', 'February', 'March' ],
-        datasets: [ { label: 'My Bar Chart', data: [40, 20, 12] } ]
+        datasets: [ { label: 'My Bar Chart', data: [40, 20, 12], backgroundColor: 'rgba(255, 99, 132, 0.2)', borderColor: 'rgb(255, 99, 132)', borderWidth: 1 } ]
       },
       chartOptions: {
         responsive: true,
